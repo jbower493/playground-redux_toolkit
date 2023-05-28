bd88c3598d939640305e97423a917955b19cf0fb
Jamie Bower <j_bower@hotmail.co.uk>
2023-05-28 17:50:38 +1000

    feat: add try catch

--- src/components/promise.tsx
+++ src/components/promise.tsx
@@ -35,12 +35,34 @@ function PromiseComponent() {
         console.log("This will still run");
     }
 
+    async function handleClickTryCatch() {
+        const newPost = {
+            id: 3,
+            title: "The third post",
+            body: "The body of the third post",
+        };
+
+        try {
+            const result = await createPost(newPost).unwrap();
+            console.log(result);
+            console.log("inside try");
+        } catch (e) {
+            console.log(e);
+            console.log("inside catch");
+        }
+
+        console.log("outside try");
+    }
+
     return (
         <div>
             <h1>Promise</h1>
             <button onClick={handleClick}>Create new post</button>
             <button onClick={handleClickAsyncAwait}>
                 Create new post async await
+            </button>
+            <button onClick={handleClickTryCatch}>
+                Create new post try catch
             </button>
             <h4>Posts:</h4>
             <ul>
